a8e5db43178adcfed3f983d27cf0a29c5cfe1630
Elsayed Rakha <elsayed.zakaria.rakha@gmail.com>
2026-09-15 15:39:38 +0300

    Add inventory system project files

---
+++ innventory system/work/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'inventory-app-v6';
+const APP_FILES = [
+  './واجهة_جرد_المخازن.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (new URL(event.request.url).origin === self.location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => event.request.mode === 'navigate' ? caches.match('./واجهة_جرد_المخازن.html') : Response.error())));
+});
